1da825b7f09ecdc76ae0bbdafea40e8fdef14cf8
zcohen331 <37035791+zcohen331@users.noreply.github.com>
2019-10-28 15:12:39 +0200

    Delete ip_connect.bash

--- ip_connect.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for ipaddr in `cat /home/terra/expect/listip.txt`;do
-	#/home/terra/expect/copy_script.exp $ipaddr jenkins mavenir scp
-	/home/terra/expect/connect.exp $ipaddr jenkins mavenir ssh
-	
-done
